093cdd605ce485e999fa101e9454a6561ee5e103
fatih-bilgin <47504617+fatih-bilgin@users.noreply.github.com>
2021-05-11 17:36:19 +0300

    date and hour separated

--- ETL/saatlik_ortalamalar.sql
+++ ETL/saatlik_ortalamalar.sql
@@ -5,19 +5,20 @@ Dönüşümler aşağıdaki sorgu ile yapıldı.
 ******/
 with cte as
 (
-SELECT SUBSTRING(convert(varchar, ZAMAN, 20),0,14) as zaman,
+SELECT SUBSTRING(convert(varchar, ZAMAN, 20),0,11) as tarih,
+	   SUBSTRING(convert(varchar, ZAMAN, 20),12,2) as saat,
 		cast(SICAKLIK as float) as sicaklik,
 		cast(NEM as float) as nem,
 		cast(RUZGAR_HIZI as float) as ruzgar,
 		cast(YAGIS as float) as yagis
 		--,cast(ZAMAN as datetime) as zaman2
-FROM [besiktas].[dbo].[havadurumu]
+FROM [zemin].[dbo].[besiktashavadurumu]
 ) 
-select zaman,
+select tarih,saat,
 	AVG(sicaklik) as ort_sicaklik,
 	AVG(nem) as ort_nem,
 	AVG(ruzgar) as ort_ruzgar,
 	AVG(yagis) as ort_yagis
 FROM cte
-GROUP BY zaman
-ORDER BY zaman
+GROUP BY tarih,saat
+ORDER BY tarih,saat
